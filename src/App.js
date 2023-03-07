a253f3b39e1a630a21367c4f2a885e67188bf2f3
kejun <kejun@alibaba-inc.com>
2023-03-07 19:12:30 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://img.alicdn.com/imgextra/i2/O1CN01LuqJvw25MY3tzh6vS_!!6000000007512-2-tps-2000-2000.png" className="App-logo" alt="logo" />
       </header>
     </div>
   );
